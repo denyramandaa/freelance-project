a46c90cbffe87c76e1ab076b0533d36fcae69170
hendydwip <hendy.putranto@kompas.com>
2022-08-05 11:23:20 +0700

    update halaman corporate

---
+++ pages/corporate/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="max-w-6xl mx-auto">
+
+    <div class="mb-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div>
+        <h1 class="text-3xl px-4 lg:px-0">MAGROF</h1>
+      </div>
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:col-span-2">
+        <div class="order-last lg:order-none">
+          <p class="px-4 lg:px-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+          </p>
+        </div>
+        <div class="flex w-full justify-center items-center">
+          <img src="https://via.placeholder.com/250" alt="placeholder">
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div class="lg:col-start-2">
+        <h1 class="text-3xl px-4 lg:px-0">OUR MANAGEMENT</h1>
+      </div>
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:col-span-2">
+        <div class="order-last">
+          <p class="px-4 lg:px-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+          </p>
+        </div>
+        <div class="flex w-full justify-center items-center">
+          <img src="https://via.placeholder.com/250" alt="placeholder">
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div>
+        <h1 class="text-3xl px-4 lg:px-0">WHY YOU SHOULD CHOOSE US</h1>
+      </div>
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:col-span-2">
+        <div class="order-last lg:order-none">
+          <p class="px-4 lg:px-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+          </p>
+        </div>
+        <div class="flex w-full justify-center items-center">
+          <img src="https://via.placeholder.com/250" alt="placeholder">
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full h-6 border mb-8">
+      box swiper harusnya
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
